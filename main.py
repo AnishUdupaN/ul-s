4d589105e9e8f1dbe3cc4d792d6588021e2d9559
AnishUdupaN <anish@email>
2025-07-09 21:53:50 +0530

    changed something

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import subprocess
+"""import subprocess
 import os
 import multi
 from ulauncher.api.client.Extension import Extension
@@ -26,6 +26,58 @@ class KeywordQueryEventListener(EventListener):
                 on_enter=RunScriptAction(f'xdg-open "www.{lines[i]}.com"', [])
             ))
         return RenderResultListAction(items[:5])
+
+if __name__ == '__main__':
+    ClipboardHistoryExtension().run()
+    
+"""
+    
+
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent, PreferencesEvent, PreferencesUpdateEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+
+class ClipboardHistoryExtension(Extension):
+
+    def __init__(self):
+        super(ClipboardHistoryExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(PreferencesEvent, PreferencesEventListener())
+        self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
+
+
+class PreferencesEventListener(EventListener):
+    def on_event(self, event, extension):
+        extension.preferences.update(event.preferences)
+
+
+class PreferencesUpdateEventListener(EventListener):
+    def on_event(self, event, extension):
+        extension.preferences[event.id] = event.new_value
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        query = event.get_argument() or ""
+        num_entries = int(extension.preferences.get('num_entries', 10))
+
+        icon_path = 'images/cliplight.png'
+        
+        items.append(ExtensionResultItem(
+            icon=icon_path,
+            name=query,
+            description="Press Enter to copy",
+            on_enter=CopyToClipboardAction(query)
+        ))
+        return RenderResultListAction(items[:num_entries])
+
 
 if __name__ == '__main__':
     ClipboardHistoryExtension().run()
