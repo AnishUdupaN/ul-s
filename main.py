79b5b8eeca8d523a9c07203574796d5454dec55b
AnishUdupaN <anish@email>
2025-07-09 22:16:01 +0530

    changed something

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
-class ClipboardHistoryExtension(Extension):
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+class mainfn(Extension):
     def __init__(self):
-        super(ClipboardHistoryExtension, self).__init__()
+        super(mainfn, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
@@ -20,12 +20,20 @@ class KeywordQueryEventListener(EventListener):
                 icon='images/icon.png',
                 name=lstt[i],
                 description="Click to Open",
-                on_enter=RunScriptAction(f'xdg-open "www.{lstt[i]}.com"', [])
+                on_enter=ExtensionCustomAction({'item': item}, keep_app_open=False)
             ))
         return RenderResultListAction(items[:5])
 
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        data = event.get_data()
+        item = data.get('item')
+        with open('~/temppp/a.txt','a') as filee:
+            filee.write(f"got {item}\n")
+            filee.close()
+    
 if __name__ == '__main__':
-    ClipboardHistoryExtension().run()
+    mainfn().run()
     
 
 
