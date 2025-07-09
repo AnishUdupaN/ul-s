cdca1b2f1bd0e35abc443fadf45fe861314934e3
AnishUdupaN <anish@email>
2025-07-09 22:08:45 +0530

    this works

--- main.py
+++ main.py
@@ -28,41 +28,4 @@ if __name__ == '__main__':
     ClipboardHistoryExtension().run()
     
 
-    
-"""
-from ulauncher.api.client.Extension import Extension
-from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
-from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
-
-class ClipboardHistoryExtension(Extension):
-
-    def __init__(self):
-        super(ClipboardHistoryExtension, self).__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-
-
-class KeywordQueryEventListener(EventListener):
-
-    def on_event(self, event, extension):
-        items = []
-        query = event.get_argument() or ""
-        num_entries = int(extension.preferences.get('num_entries', 10))
-
-        icon_path = 'images/cliplight.png'
-        
-        items.append(ExtensionResultItem(
-            icon=icon_path,
-            name=query,
-            description="Press Enter to copy",
-            on_enter=CopyToClipboardAction(query)
-        ))
-        return RenderResultListAction(items[:num_entries])
-
-
-if __name__ == '__main__':
-    ClipboardHistoryExtension().run()
-"""
